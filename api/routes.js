3771f239c690b95e844247d42b8b5fe0589748ce
easauceda <easauceda@gmail.com>
2015-12-02 10:56:58 -0800

    added unique identifier

--- api/routes.js
+++ api/routes.js
@@ -1,6 +1,7 @@
 var couchbase = require('couchbase');
 var cluster = new couchbase.Cluster('couchbase://127.0.0.1');
 var bucket = cluster.openBucket('reminders');
+var uuid = require('node-uuid');
 
 //Add a reminder
 module.exports = function(app) {
@@ -12,12 +13,13 @@ module.exports = function(app) {
         }
 
           // Convert our form input into JSON ready to store in Couchbase
-          var json = JSON.stringify(req.query);
-          
+          var uid = uuid.v1(); 
+          req.query.id = uid;
+          var json = JSON.stringify(req.query); 
           console.log(json);
 
           // Save it into Couchbase with keyname user
-          bucket.insert(Math.random().toString(), json, function (err, response){
+          bucket.insert(uid, json, function (err, response){
             if (err) {
                   console.log('Failed to save to Couchbase', err);
                   return;
