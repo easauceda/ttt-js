28c6161cd9058aaa918b7695d23818e75afaa855
tlee93 <tlee9383@gmail.com>
2015-12-03 20:45:59 -0800

    fixed query

--- api/routes.js
+++ api/routes.js
@@ -5,40 +5,42 @@ var uuid = require('node-uuid');
 
 //Add a reminder
 module.exports = function(app) {
-    app.get('/api', function(req, res) {
-        console.log(req.query);
-        //console.log(req.query.carrier);
-        if (!req.query){
-            return res.sendStatus(400);
-        }
-
-          // Convert our form input into JSON ready to store in Couchbase
-          var uid = uuid.v1(); 
-          req.query.id = uid;
-          var json = JSON.stringify(req.query); 
-          console.log(json);
-
-          // Save it into Couchbase with keyname user
-          bucket.insert(uid, json, function (err, response){
-            if (err) {
-                  console.log('Failed to save to Couchbase', err);
-                  return;
-            } else {
-                  res.send('Saved to Couchbase!');
+        app.get('/api', function(req, res) {
+            console.log(req.query);
+            //console.log(req.query.carrier);
+            if (!req.query) {
+                return res.sendStatus(400);
             }
-          });
-    });
-    app.get('/api/reminder', function(req, res) {
-    	//couchbase code here
-	    var query = ViewQuery.from('dev_by_number')
-           .key(res.params.$number)
-           .stale(ViewQuery.Update.BEFORE);
-          res.json({'reminders': ???
-        })
-    });
-    app.get('/api/delete/:id', function(req, res) {
-	    //couchbase call here
-	    console.log(req.params.id);
-	    res.send("OK");
-    });
-};
+
+            // Convert our form input into JSON ready to store in Couchbase
+            var uid = uuid.v1();
+            req.query.id = uid;
+            var json = JSON.stringify(req.query);
+            console.log(json);
+
+            // Save it into Couchbase with keyname user
+            bucket.insert(uid, json, function(err, response) {
+                if (err) {
+                    console.log('Failed to save to Couchbase', err);
+                    return;
+                } else {
+                    res.send('Saved to Couchbase!');
+                }
+            });
+        });
+        app.get('/api/reminder', function(req, res) {
+                //couchbase code here
+                var query = ViewQuery.from('dev_by_number')
+                    .key(req.params.$number)
+                    .stale(ViewQuery.Update.BEFORE);
+                bucket.query(query, function(err, results) {
+                        res.json({
+                            'reminders': results
+                        })
+                    };
+                }); app.get('/api/delete/:id', function(req, res) {
+                //couchbase call here
+                console.log(req.params.id);
+                res.send("OK");
+            });
+        };
