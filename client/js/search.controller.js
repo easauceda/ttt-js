b70590dbf7ca291e0c734c35b52062d835c37cef
easauceda <easauceda@gmail.com>
2015-12-03 22:11:54 -0800

    changed routes to reflect tims contributions

--- client/js/search.controller.js
+++ client/js/search.controller.js
@@ -1,7 +1,8 @@
 angular.module('search.controller', [])
 	.controller('SearchController', function($scope, SearchService) {
 		$scope.search = function() {
-			SearchService.get({$number: $scope.number}, function(response) {				$scope.reminders = response;
+			SearchService.query({$number: $scope.number}, function(response) {
+				$scope.reminders = response;
 			});
 		};
 	});
